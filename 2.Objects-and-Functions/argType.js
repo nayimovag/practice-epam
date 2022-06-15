f2c891aa7374c76764f1dc56505f696d832e6699
nayimovag <72025248+nayimovag@users.noreply.github.com>
2022-06-15 18:43:08 +0500

    Add files via upload

---
+++ 2.Objects-and-Functions/argType.js
@@ -0,0 +1,5 @@
+function calcType(argument){
+    console.log(typeof(argument));//calling a function for finding the type, then outing it into the console
+}
+
+calcType(true);
---
+++ 2.Objects-and-Functions/car.js
@@ -0,0 +1,10 @@
+//tasks 1, 2, and 3 summarized
+let car = {//created a car object
+    color: "black",//added color property
+  };
+
+  car.color = "green";//changed color property
+
+  car.showPower = function(){//added power property, which is a function that displays the engine power
+    console.log(power = '120ph')
+  };
