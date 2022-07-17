b2319e25c19b6ebc5510bdcf0167e5ba4e07c52c
nayimovag <72025248+nayimovag@users.noreply.github.com>
2022-07-17 14:39:01 +0500

    unit testing with mocha

---
+++ app.js
@@ -0,0 +1,12 @@
+class Calculator{
+    add(first, second) {
+        let result = first + second
+        return result
+    }
+    multiply(first, second) {
+        let result = first * second
+        return result
+    }
+}
+let example = new Calculator
+module.exports = example;
