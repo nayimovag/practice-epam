da0f6dea878b5b26a315d7d1decb6998354bb629
nayimovag <nayimovag@gmail.com>
2022-07-21 21:26:05 +0500

    bring it on task on wdio

---
+++ app/page-objects/pastebin-saved-paste-page.js
@@ -0,0 +1,19 @@
+const Page = require('./page')
+
+class PastebinSavedPastePage extends Page {
+
+  get getActualPageTitle() {
+    const actualPageTitle = browser.getTitle()
+    return actualPageTitle
+  }
+
+  get actualSyntaxHighlighting() {
+    return $('//div[@class="left"]//a')
+  }
+
+  get actualPastedText() {
+    return $('//textarea')
+  }
+}
+
+module.exports = new PastebinSavedPastePage()
