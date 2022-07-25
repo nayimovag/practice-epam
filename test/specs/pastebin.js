d6450c79eb86937cab2a94fc9ba3e494d472b9c5
nayimovag <nayimovag@gmail.com>
2022-07-25 14:04:59 +0500

    i can win task on wdio

--- test/specs/pastebin.js
+++ test/specs/pastebin.js
@@ -1,13 +1,11 @@
-const pastebinHomePage = require('../../app/page-objects/pastebin-home-page')
+const pastebinHomePage = require("../../app/page-objects/pastebin-home-page");
 
-describe('Pastebin.com', () => {
-    
-  const PASTE_TEXT = 'git config --global user.name "New Sheriff in Town" \ngit reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code") \ngit push origin master --force'
-  const PASTE_NAME = 'how to gain dominance among developers'
+describe("Pastebin.com", () => {
+  const PASTE_TEXT = "Hello from Webdriver";
+  const PASTE_NAME = "helloweb";
 
-  it('should login with valid credentials', async () => {
-    await pastebinHomePage.open()
-    await pastebinHomePage.typeAndSavePaste(PASTE_TEXT, PASTE_NAME)
-  })
-
-})
+  it("should login with valid credentials", async () => {
+    await pastebinHomePage.open();
+    await pastebinHomePage.typeAndSavePaste(PASTE_TEXT, PASTE_NAME);
+  });
+});
