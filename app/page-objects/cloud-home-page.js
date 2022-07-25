146da4b926ecb6cbdf5c849ea026be238591af7a
nayimovag <nayimovag@gmail.com>
2022-07-25 14:59:20 +0500

    hardcore task on wdio

--- app/page-objects/cloud-home-page.js
+++ app/page-objects/cloud-home-page.js
@@ -9,16 +9,6 @@ class CloudHomePage extends Page {
     return $('//a[contains(.,"Google Cloud Pricing Calculator")]');
   }
 
-  get homePageFirstFrame() {
-    return $(
-      "//iframe[contains(@src,'https://cloud.google.com/products/calculator/index_d6a98ba38837346d20babc06ff2153b68c2990fa24322fe52c5f83ec3a78c6a0.frame')]"
-    );
-  }
-
-  get homePageSecondFrame() {
-    return $("#myFrame");
-  }
-
   get numberOfInstancesArea() {
     return $("//input[@ng-model='listingCtrl.computeServer.quantity']");
   }
